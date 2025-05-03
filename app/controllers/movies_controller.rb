19448e98b02762920ccc10914d7b153b6cbf4a57
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-05-03 15:04:34 +0500

    feat: integration with the omdb API results shown on search.html.erb

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,49 @@
+require "json"
+require "net/http"
+require "cgi"
+
+class MoviesController < ApplicationController
+  OMDB_API_KEY = CGI.escape(ENV["OMDB_API_KEY"])
+  OMDB_BASE_URL = "http://www.omdbapi.com/"
+
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render json: @movie
+  end
+
+  def search
+    query  = params[:q]
+
+    @local_results = Movie.where("title LIKE ?", "%#{query}%")
+
+    if @local_results.empty? || @local_results.size <=5
+       uri = URI(OMDB_BASE_URL)
+       uri.query = URI.encode_www_form({
+        apikey: OMDB_API_KEY,
+        s: query
+      })
+
+      response = Net::HTTP.get_response(uri)
+
+      if response.is_a?(Net::HTTPSuccess)
+        omdb_data = JSON.parse(response.body)
+
+        if omdb_data["Response"] == "True"
+          @omdb_results = omdb_data["Search"]
+        else
+          @omdb_results = [ "No Res" ]
+        end
+      else
+        @omdb_results = [ "Error: #{response.code} - #{response.body}" ]
+      end
+    else
+      @omdb_results = [ "No Local Res" ]
+    end
+
+  end
+end
